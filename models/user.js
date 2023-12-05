a079b19cec0c1edddbc0e6ad049d7b5b22ab3ac6
jskartes <joshuakartes@gmail.com>
2023-12-05 16:33:55 -0700

    User model redesign

--- models/user.js
+++ models/user.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  isAdmin: Boolean,
+  role: {
+    type: String,
+    enum: ['admin', 'user']
+  },
   name: String,
   googleId: {
     type: String,
     required: true
   },
   email: String,
-  avatar: String
 }, {
   timestamps: true
 });
