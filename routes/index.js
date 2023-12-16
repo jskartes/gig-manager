0029192fe7c4ad0266f648d13658d22537858b1c
jskartes <joshuakartes@gmail.com>
2023-12-15 17:29:24 -0700

    initial Heroku deployment

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,29 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.get('/', (req, res, next) => res.render('index'));
-
-router.get('/auth/google', passport.authenticate(
-  'google',
-  { scope: ['profile', 'email'], prompt: 'select_account' }
-));
-
-router.get('/oauth2callback', passport.authenticate(
-  'google',
-  { successRedirect: '/users', failureRedirect: '/' }
-));
-
-router.get('/logout', (req, res) => {
-  req.logout(() => res.redirect('/'));
-});
+router.get(
+  '/',
+  (req, res, next) => res.render('index')
+);
+router.get(
+  '/auth/google',
+  passport.authenticate(
+    'google',
+    { scope: ['profile', 'email'], prompt: 'select_account' }
+  )
+);
+router.get(
+  '/oauth2callback',
+  passport.authenticate(
+    'google',
+    { successRedirect: '/users', failureRedirect: '/' }
+  )
+);
+router.get(
+  '/logout',
+  (req, res) => { 
+    req.logout(() => res.redirect('/'));
+  }
+);
 
 module.exports = router;
